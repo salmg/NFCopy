7a69a7345e80cf16c6368ce791e4f766a3e82298
Salvador Mendoza <salvador_m_g@msn.com>
2019-06-19 05:42:57 -0700

    Delete README.h

--- NFCopy85/README.h
+++
@@ -1,3 +0,0 @@
-By default, the Adafruit PN532 library is too big for the ATtiny85 and some functions had to be edited for a good communication between the ATtiny85 and PN532 board. To establish the communication, I decided to use SPI. So I deleted all the parts in the library that were not needed for the SPI communication.
-
-More details about this project: https://salmg.net/2019/06/16/nfcopy85/
